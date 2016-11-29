94420ff469d1df6622248b30bf0675d5955960c2
AmirJ <college.amir.javeri@gmail.com>
2016-11-29 13:31:24 -0600

    baseplate search

---
+++ SearchController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LonghornMusic.Models;
+//using LonghornMusic.Models.IdentityModels.AppDbContext;
+
+
+namespace longhornmusic.Controllers
+{
+    public class SearchController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+        // GET: Search
+        public ActionResult Index(string SearchString)
+        {
+            List<Song> SelectedSongs = new List<Song>();
+            List<Song> AllSongs = new List<Song>();
+
+            if (SearchString == null || SearchString == "")
+            {
+                var allcount = db.Songs.Count();
+                AllSongs = db.Songs.Where(c => c.SongName.Contains("")).ToList();
+                string countstring = "Displaying " + allcount + " Records";
+                ViewBag.countstring = countstring;
+                return View(AllSongs.OrderBy(c => c.SongName).ThenBy(c => c.SongPrice).ToList());
+            }
+            else
+            {
+                SelectedSongs = db.Songs.Where(c => c.SongName.Contains(SearchString)).ToList();
+                SelectedSongs.OrderBy(c => c.SongName).ThenBy(c => c.SongPrice);
+
+                var viewcount = SelectedSongs.Count();
+                var allcount = db.Songs.Count();
+
+                string countstring = "Displaying " + viewcount + " of " + allcount + " Records";
+                ViewBag.countstring = countstring;
+                return View(SelectedSongs.OrderBy(c => c.SongName).ThenBy(c => c.SongPrice).ToList());
+            }
+            
+        }
+
+    }
+}
